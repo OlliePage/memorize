320e0f5de3ac51e13e4bd930b0be48bddcd2762b
ollie <products15816@gmail.com>
2022-11-14 22:49:02 +0000

    finish L4

--- Lecture3/LectureMemorize/MemoryGame.swift
+++ Lecture3/LectureMemorize/MemoryGame.swift
@@ -9,40 +9,51 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
     
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
-        print("\(cards)")
-    }
-    
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if cards[index].id == card.id {
-                return index
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}),
+               !cards[chosenIndex].isFaceUp,
+               !cards[chosenIndex].isMatched {
+            if let potentialMatch = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatch].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatch].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
             }
+            
+            cards[chosenIndex].isFaceUp.toggle()
+            
         }
-        return 0
+        print("Couldn't find card")
     }
     
-    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
-        cards = []
-        // add numberOfPairsOfCards * 2 cards to cards array
-        for pairIndex in 0..<numberOfPairsOfCards {
-            let content = createCardContent(pairIndex)
-            cards.append(Card(content: content, id: pairIndex*2))
-            cards.append(Card(content: content, id: pairIndex*2+1))
-        }
-        
+
+init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+    cards = []
+    // add numberOfPairsOfCards * 2 cards to cards array
+    for pairIndex in 0..<numberOfPairsOfCards {
+        let content = createCardContent(pairIndex)
+        cards.append(Card(content: content, id: pairIndex*2))
+        cards.append(Card(content: content, id: pairIndex*2+1))
     }
     
-    struct Card: Identifiable {
-        var isFaceUp: Bool = true
-        var isMatched: Bool = false
-        var content: CardContent
-        var id: Int
-    }
-    
+}
+
+struct Card: Identifiable {
+    var isFaceUp: Bool = false
+    var isMatched: Bool = false
+    var content: CardContent
+    var id: Int
+}
+
 }
